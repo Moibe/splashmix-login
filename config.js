21557e0d2d46948830c9fcf8d0a766ff68ec10e7
Moibe <moi.estrello@gmail.com>
2025-08-01 16:06:19 -0600

    Keys are public

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBSlzBn-wxB4PrldzfAF8_xtEW2E6HIqJ8",
+  authDomain: "splashmix-ai.firebaseapp.com",
+  projectId: "splashmix-ai",
+  storageBucket: "splashmix-ai.firebasestorage.app",
+  messagingSenderId: "1093510950901",
+  appId: "1:1093510950901:web:7f2e18ad5f959af42d7abc",
+  measurementId: "G-V2L2EG9LPZ"
+};
